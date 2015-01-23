4b99f65f4478a6c04f31601bbce61a5057b3ac8a
shutiao <xuhaihua90@gmail.com>
2015-01-23 05:53:38 +0000

    Updated the index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('This is Hadison and Katie's home.')
+  response.send('Hadison')
 })
 
 app.listen(app.get('port'), function() {
