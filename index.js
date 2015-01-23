208e180b802683f70c0fbdce2950540e6bcbfee8
shutiao <xuhaihua90@gmail.com>
2015-01-23 17:41:09 +0000

    Updated the index file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var data = fs.readFileSync('./index.html', "utf-8")
+
 app.get('/', function(request, response) {
-  response.send('Hadison & Katie')
+  response.send(data)
 })
 
 app.listen(app.get('port'), function() {
